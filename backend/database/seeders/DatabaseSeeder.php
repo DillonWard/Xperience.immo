8247bbb59b802332888f412d312b175f7def2f19
Dillon Ward <Dillonward13@gmail.com>
2021-08-19 00:08:15 +0200

    removing unused code

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,20 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Factory(\App\\Models\Agency::class, 10)->create
         \App\Models\Agency::factory(3)->create();
-        \App\Models\Programme::factory(10)->create();
-        // factory(\App\Models\Agency::class, 3)->create()->each(function ($agency) {
-        //     // Seed the relation with one address
-        //     $programme = factory(\App\Models\Programme::class, 3)->make();
-        //     $agency->address()->save($programme);
-        // });
-
-        // $this->call([
-        //     AgencySeeder::class,
-        //     ProgrammeSeeder::class,
-        // ]);
-    
-
+        \App\Models\Programme::factory(10)->create();  
     }
 }
